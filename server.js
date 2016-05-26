2cf79ab56180195342f597c94637fd837a455e1c
CK <thecurrencykid@gmail.com>
2016-05-26 16:42:08 -0700

    adding todos by hand

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser'); 
 var app = express();
 var PORT = process.env.PORT || 3000; 
 
-var todos = [{
-  id: 1, 
-  description: 'Meet with boss', 
-  completed: false
-}, {
-  id: 2, 
-  description: 'Go to the store', 
-  completed: false
-}, {
-  id: 3,
-  description: "Feed dog",
-  completed:true 
-}];
+var todos = [];
+var todoNextId = 1; 
+
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
   res.send('Todo API Root'); 
@@ -42,6 +34,23 @@ app.get('/todos/:id', function(req,res){
     res.status(404).send(); 
   }
 });
+
+// POST create new todos ... /todos
+// need body-parser for this
+app.post('/todos', function(req,res){
+  var body = req.body;  
+
+  console.log('description: ' + body.description); 
+  
+  // add id field
+  body.id = todoNextId++;
+
+  //push onto todos arrway
+  todos.push(body); 
+
+  res.json(body); 
+});
+
 
 app.listen(PORT, function(){
   console.log("Express listening on port " + PORT) ; 
