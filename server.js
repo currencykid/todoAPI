e9af7e069a1c8508fe8797b4162bb9d1054729d8
CK <thecurrencykid@gmail.com>
2016-05-26 17:59:47 -0700

    validations

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser'); 
+var _ = require('underscore'); 
+
 var app = express();
 var PORT = process.env.PORT || 3000; 
-
 var todos = [];
 var todoNextId = 1; 
 
@@ -20,13 +21,7 @@ app.get('/todos', function(req,res){
 // GET /todos/:id 
 app.get('/todos/:id', function(req,res){
   var todoId = parseInt(req.params.id, 10); 
-  var matchedTodo; 
-
-  todos.forEach(function(todo){
-    if (todoId === todo.id) {  
-      matchedTodo = todo; 
-    } 
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
 
   if (matchedTodo){
     res.json(matchedTodo); 
@@ -38,10 +33,14 @@ app.get('/todos/:id', function(req,res){
 // POST create new todos ... /todos
 // need body-parser for this
 app.post('/todos', function(req,res){
-  var body = req.body;  
+  var body = _.pick(req.body, 'description', 'completed' );  
 
-  console.log('description: ' + body.description); 
-  
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) { 
+    return res.status(400).send();  
+  }
+
+  //body.descripotion to trimmed value
+  body.description = body.description.trim(); 
   // add id field
   body.id = todoNextId++;
 
