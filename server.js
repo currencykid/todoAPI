e943e108682eb530f27357276a7e264312c93eba
CK <thecurrencykid@gmail.com>
2016-06-01 13:22:53 -0700

    add auth token generation to POST /users/login

--- server.js
+++ server.js
@@ -109,15 +109,15 @@ app.put('/todos/:id', function(req,res){
       }, function(e) {
         res.status(400).json(e); 
       }); 
-      } else {
-        res.status(404).send(); 
-      }
-    }, function(){
-      res.status(500).send(); 
-    });
-  }); 
+    } else {
+      res.status(404).send(); 
+    }
+  }, function(){
+    res.status(500).send(); 
+  });
+}); 
 
-  /////////////// USERS REQUESTS
+/////////////// USERS REQUESTS
 
 app.post('/users', function(req,res){
   var body = _.pick(req.body, 'email', 'password' );  
@@ -134,9 +134,15 @@ app.post('/users/login', function(req,res){
   var body = _.pick(req.body, 'email', 'password' );  
 
   db.user.authenticate(body).then(function(user){
-    res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
+    var token = user.generateToken('authentication'); 
+
+    if (token) {
+      res.header('Auth', token ).json(user.toPublicJSON());
+    } else {
+      res.status(401).send(); 
+    } 
   }, function(e){
-    res.status(400).json(e); 
+    res.status(401).send(); 
   });
 });
 
